5320417108b8dd5919141fa15dc3fbe1fa2f9028
gowthaam <ulimit15@gmail.com>
2018-07-06 22:41:20 +0530

    without context

--- Tomato/src/main/java/com/AppController.java
+++ Tomato/src/main/java/com/AppController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -39,7 +40,8 @@ OrderRepository orderrepository;
 @Autowired 
 ReviewRepository reviewrepository;
 public  Map<String,ArrayList<OrderDetails>> order = new HashMap<String,ArrayList<OrderDetails>>();
-
+@Value("${context}")
+String context;
 
 public static String hotelName = new String("");
 public static String username = new String("");
